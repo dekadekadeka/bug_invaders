ac11ef8ab76bab8a0a18341e0fed08d3d4e0dc5e
github username <dekadekadeka@gmail.com>
2019-06-18 08:53:53 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :comments, only: [:index]
-      resources :games, only: [:index]
-      resources :users, only: [:index]
+      resources :comments, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+      resources :games, only: [:index, :show, :destroy]
+      resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     end
   end
 end
