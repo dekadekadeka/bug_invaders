a7cb5f34c3ff35327c797ae39d8b17415f839f04
github username <dekadekadeka@gmail.com>
2019-06-18 09:06:43 -0400

    changed routes again

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :comments
+      resources :comments, only: [:index, :new, :create, :edit, :update, :destroy]
       resources :games, only: [:index, :destroy]
       resources :users
     end
