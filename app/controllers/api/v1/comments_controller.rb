3a9a971df12e28e4e90e6a2cb3ac6c1cb71decf3
github username <dekadekadeka@gmail.com>
2019-06-18 11:51:50 -0400

    whole backend

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,4 +3,33 @@ class Api::V1::CommentsController < ApplicationController
         @comments = Comment.all
         render json: @comments
     end
+
+    def create
+        @comment = Comment.new(comment_params)
+        if(@comment.save)
+            render json: @comment
+        else
+            render json: { errors: @comment.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        if (@comment.update(comment_params))
+            render json: @comment, status: :accepted
+        else
+            render json: { errors: @comment.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:user_id, :content, :game_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :comments, only: [:index, :new, :create, :edit, :update, :destroy]
-      resources :games, only: [:index, :destroy]
-      resources :users
+      resources :comments, only: [:index, :create, :update, :destroy]
+      resources :games, only: [:index, :create, :destroy]
+      resources :users, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
